cea20626f260e092cc60219c66120f9b62f45905
Arnold Noronha <arnold@screenshotbot.io>
2026-01-09 14:58:17 -0500

    Simplify the copy directory logic so we don't have do a rmtree
    Summary: The rmtree might end up being dangerous code if it's
    translated by AI to Kotlin
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:

--- plugin/src/py/android_screenshot_tests/pull_screenshots.py
+++ plugin/src/py/android_screenshot_tests/pull_screenshots.py
@@ -337,24 +337,10 @@ def android_path_join(a, *args):
 
 def pull_images(dir, device_dir, test_run_id, adb_puller):
     if adb_puller.remote_file_exists(android_path_join(device_dir, test_run_id)):
-        bundle_name_local_file = join(dir, os.path.basename(test_run_id))
-
         # Optimization to pull down all the screenshots in a single pull.
         # If this file exists, we assume all of the screenshots are inside it.
         adb_puller.pull_folder(
-            android_path_join(device_dir, test_run_id), bundle_name_local_file
-        )
-
-        move_all_files_to_different_directory(bundle_name_local_file, dir)
-        # clean up
-        shutil.rmtree(bundle_name_local_file)
-
-
-def move_all_files_to_different_directory(source_dir, target_dir):
-    file_names = os.listdir(source_dir)
-    for file_name in file_names:
-        shutil.move(
-            os.path.join(source_dir, file_name), os.path.join(target_dir, file_name)
+            android_path_join(device_dir, test_run_id), dir
         )
 
 
