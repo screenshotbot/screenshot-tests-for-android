a3c99e17ae84c66b20612ac70c14264b24a02141
Arnold Noronha <arnold@screenshotbot.io>
2026-01-09 18:07:01 -0500

    Extract verify_helper out of Recorder
    Summary:
    Since this is the last thing done in Recorder.verify, it
    makes for a relatively easy transition to Kotlin.
    
    Differential Revision: https://phabricator.tdrhq.com/D12198

--- plugin/src/py/android_screenshot_tests/recorder.py
+++ plugin/src/py/android_screenshot_tests/recorder.py
@@ -29,6 +29,24 @@ from . import common
 class VerifyError(Exception):
     pass
 
+def _is_image_same(file1, file2, failure_file):
+    with Image.open(file1) as im1, Image.open(file2) as im2:
+        diff_image = ImageChops.difference(im1.convert("RGB"), im2.convert("RGB"))
+        try:
+            diff = diff_image.getbbox()
+            if diff is None and im1.size == im2.size:
+                return True
+            else:
+                if failure_file:
+                    diff_list = list(diff) if diff else []
+                    draw = ImageDraw.Draw(im2)
+                    draw.rectangle(diff_list, outline=(255, 0, 0))
+                    im2.save(failure_file)
+                return False
+        finally:
+            diff_image.close()
+
+
 
 class Recorder:
     def __init__(self, input, output, failure_output):
@@ -87,23 +105,6 @@ class Recorder:
             shutil.rmtree(self._output)
         os.makedirs(self._output)
 
-    def _is_image_same(self, file1, file2, failure_file):
-        with Image.open(file1) as im1, Image.open(file2) as im2:
-            diff_image = ImageChops.difference(im1.convert("RGB"), im2.convert("RGB"))
-            try:
-                diff = diff_image.getbbox()
-                if diff is None and im1.size == im2.size:
-                    return True
-                else:
-                    if failure_file:
-                        diff_list = list(diff) if diff else []
-                        draw = ImageDraw.Draw(im2)
-                        draw.rectangle(diff_list, outline=(255, 0, 0))
-                        im2.save(failure_file)
-                    return False
-            finally:
-                diff_image.close()
-
     def record(self):
         self._clean()
         self._record()
@@ -111,33 +112,44 @@ class Recorder:
     def verify(self):
         self._output = tempfile.mkdtemp()
         self._record()
+        verify_helper(
+            screenshots=self._get_metadata_json(),
+            output=self._output,
+            expected_output=self._realoutput,
+            failure_output=self._failure_output)
 
-        screenshots = self._get_metadata_json()
-        failures = []
-        for screenshot in screenshots:
-            name = screenshot["name"] + ".png"
-            actual = join(self._output, name)
-            expected = join(self._realoutput, name)
-            if self._failure_output:
-                diff_name = screenshot["name"] + "_diff.png"
-                diff = join(self._failure_output, diff_name)
 
-                if not self._is_image_same(expected, actual, diff):
-                    expected_name = screenshot["name"] + "_expected.png"
-                    actual_name = screenshot["name"] + "_actual.png"
+def verify_helper(screenshots, output, expected_output, failure_output):
+    """
+    screenshots is is the parsed json from metadata.json
+    output, expected_output are the directories with the screenshots
+    failure_output is where we copy the failure screenshots to.
+    """
+    failures = []
+    for screenshot in screenshots:
+        name = screenshot["name"] + ".png"
+        actual = join(output, name)
+        expected = join(expected_output, name)
+        if failure_output:
+            diff_name = screenshot["name"] + "_diff.png"
+            diff = join(failure_output, diff_name)
 
-                    shutil.copy(actual, join(self._failure_output, actual_name))
-                    shutil.copy(expected, join(self._failure_output, expected_name))
+            if not _is_image_same(expected, actual, diff):
+                expected_name = screenshot["name"] + "_expected.png"
+                actual_name = screenshot["name"] + "_actual.png"
 
-                    failures.append((expected, actual))
-            else:
-                if not self._is_image_same(expected, actual, None):
-                    raise VerifyError("Image %s is not same as %s" % (expected, actual))
+                shutil.copy(actual, join(failure_output, actual_name))
+                shutil.copy(expected, join(failure_output, expected_name))
 
-        if failures:
-            reason = ""
-            for expected, actual in failures:
-                reason = reason + "\nImage %s is not same as %s" % (expected, actual)
-            raise VerifyError(reason)
+                failures.append((expected, actual))
+        else:
+            if not _is_image_same(expected, actual, None):
+                raise VerifyError("Image %s is not same as %s" % (expected, actual))
 
-        shutil.rmtree(self._output)
+    if failures:
+        reason = ""
+        for expected, actual in failures:
+            reason = reason + "\nImage %s is not same as %s" % (expected, actual)
+        raise VerifyError(reason)
+
+    shutil.rmtree(output)
