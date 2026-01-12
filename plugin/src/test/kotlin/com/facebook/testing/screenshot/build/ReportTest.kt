90e28aaaba58398a60143db0a2b633be79275571
Arnold Noronha <arnold@screenshotbot.io>
2026-01-12 10:15:23 -0500

    ...

--- plugin/src/test/kotlin/com/facebook/testing/screenshot/build/ReportTest.kt
+++ plugin/src/test/kotlin/com/facebook/testing/screenshot/build/ReportTest.kt
@@ -16,8 +16,14 @@
 
 package com.facebook.testing.screenshot.build
 
+import com.google.gson.JsonArray
+import com.google.gson.JsonObject
+import java.awt.Color
+import java.awt.image.BufferedImage
 import java.io.File
+import javax.imageio.ImageIO
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -88,5 +94,271 @@ class ReportTest {
       "File should have different content",
       cssFile.length() != originalLength
     )
+  }
+
+  @Test
+  fun testGenerateHtml_createsIndexHtml() {
+    createTestMetadata(
+      listOf(
+        Triple("com.foo.ScriptsFixtureTest_testGetTextViewScreenshot", 1, 1)
+      )
+    )
+    createTestImage("com.foo.ScriptsFixtureTest_testGetTextViewScreenshot.png", 10, 10, Color.BLUE)
+
+    val htmlFile = generateHtml(outputDir)
+
+    assertTrue("index.html should exist", htmlFile.exists())
+    assertEquals("index.html", htmlFile.name)
+  }
+
+  @Test
+  fun testGenerateHtml_returnsValidFile() {
+    createTestMetadata(
+      listOf(
+        Triple("com.foo.ScriptsFixtureTest_testGetTextViewScreenshot", 1, 1),
+        Triple("com.foo.ScriptsFixtureTest_testSecondScreenshot", 1, 1)
+      )
+    )
+    createTestImage("com.foo.ScriptsFixtureTest_testGetTextViewScreenshot.png", 10, 10, Color.BLUE)
+    createTestImage("com.foo.ScriptsFixtureTest_testSecondScreenshot.png", 10, 10, Color.RED)
+
+    val html = generateHtml(outputDir)
+
+    assertTrue("HTML file should exist", html.exists())
+    assertTrue("HTML file should be absolute", html.isAbsolute)
+  }
+
+  @Test
+  fun testGenerateHtml_imageIsLinked() {
+    createTestMetadata(
+      listOf(
+        Triple("com.foo.ScriptsFixtureTest_testGetTextViewScreenshot", 1, 1)
+      )
+    )
+    createTestImage("com.foo.ScriptsFixtureTest_testGetTextViewScreenshot.png", 10, 10, Color.BLUE)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue("HTML should contain package name", htmlContent.contains("com.foo"))
+    assertTrue(
+      "HTML should contain image tag",
+      htmlContent.contains("<img src=\"./com.foo.ScriptsFixtureTest_testGetTextViewScreenshot.png\"")
+    )
+  }
+
+  @Test
+  fun testGenerateHtml_containsBasicStructure() {
+    createTestMetadata(listOf(Triple("test_screenshot", 1, 1)))
+    createTestImage("test_screenshot.png", 10, 10, Color.BLUE)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue("HTML should have DOCTYPE", htmlContent.contains("<!DOCTYPE html>"))
+    assertTrue("HTML should have title", htmlContent.contains("<title>Screenshot Test Results</title>"))
+    assertTrue("HTML should include jQuery", htmlContent.contains("jquery"))
+    assertTrue("HTML should include default.css", htmlContent.contains("default.css"))
+    assertTrue("HTML should include default.js", htmlContent.contains("default.js"))
+  }
+
+  @Test
+  fun testGenerateHtml_withGroup() {
+    val metadata = JsonArray()
+    val screenshot = JsonObject()
+    screenshot.addProperty("name", "test_screenshot")
+    screenshot.addProperty("group", "my_group")
+    screenshot.addProperty("tileWidth", 1)
+    screenshot.addProperty("tileHeight", 1)
+    metadata.add(screenshot)
+
+    val metadataFile = File(outputDir, "metadata.json")
+    metadataFile.writeText(metadata.toString())
+    createTestImage("test_screenshot.png", 10, 10, Color.BLUE)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue("HTML should contain group", htmlContent.contains("my_group"))
+    assertTrue("HTML should have screenshot_group div", htmlContent.contains("screenshot_group"))
+  }
+
+  @Test
+  fun testGenerateHtml_withDescription() {
+    val metadata = JsonArray()
+    val screenshot = JsonObject()
+    screenshot.addProperty("name", "test_screenshot")
+    screenshot.addProperty("description", "This is a test description")
+    screenshot.addProperty("tileWidth", 1)
+    screenshot.addProperty("tileHeight", 1)
+    metadata.add(screenshot)
+
+    val metadataFile = File(outputDir, "metadata.json")
+    metadataFile.writeText(metadata.toString())
+    createTestImage("test_screenshot.png", 10, 10, Color.BLUE)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue("HTML should contain description", htmlContent.contains("This is a test description"))
+    assertTrue(
+      "HTML should have screenshot_description div",
+      htmlContent.contains("screenshot_description")
+    )
+  }
+
+  @Test
+  fun testGenerateHtml_withError() {
+    val metadata = JsonArray()
+    val screenshot = JsonObject()
+    screenshot.addProperty("name", "test_screenshot")
+    screenshot.addProperty("error", "Test error message")
+    screenshot.addProperty("tileWidth", 1)
+    screenshot.addProperty("tileHeight", 1)
+    metadata.add(screenshot)
+
+    val metadataFile = File(outputDir, "metadata.json")
+    metadataFile.writeText(metadata.toString())
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue("HTML should contain error message", htmlContent.contains("Test error message"))
+    assertTrue("HTML should have screenshot_error div", htmlContent.contains("screenshot_error"))
+  }
+
+  @Test
+  fun testGenerateHtml_withTiledScreenshot() {
+    createTestMetadata(listOf(Triple("tiled_screenshot", 2, 2)))
+    createTestImage("tiled_screenshot.png", 10, 10, Color.BLUE)
+    createTestImage("tiled_screenshot_1_0.png", 10, 10, Color.RED)
+    createTestImage("tiled_screenshot_0_1.png", 10, 10, Color.GREEN)
+    createTestImage("tiled_screenshot_1_1.png", 10, 10, Color.YELLOW)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue("HTML should contain first tile", htmlContent.contains("tiled_screenshot.png"))
+    assertTrue("HTML should contain second tile", htmlContent.contains("tiled_screenshot_1_0.png"))
+    assertTrue("HTML should contain third tile", htmlContent.contains("tiled_screenshot_0_1.png"))
+    assertTrue("HTML should contain fourth tile", htmlContent.contains("tiled_screenshot_1_1.png"))
+  }
+
+  @Test
+  fun testGenerateHtml_alternateRowsStyled() {
+    createTestMetadata(
+      listOf(
+        Triple("screenshot1", 1, 1),
+        Triple("screenshot2", 1, 1),
+        Triple("screenshot3", 1, 1)
+      )
+    )
+    createTestImage("screenshot1.png", 10, 10, Color.BLUE)
+    createTestImage("screenshot2.png", 10, 10, Color.BLUE)
+    createTestImage("screenshot3.png", 10, 10, Color.BLUE)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue("HTML should have alternate class", htmlContent.contains("class=\"screenshot alternate\""))
+    assertTrue("HTML should have non-alternate class", htmlContent.contains("class=\"screenshot \""))
+  }
+
+  @Test
+  fun testSortScreenshots_withSameGroup_orderedTogether() {
+    val screenshots = JsonArray()
+
+    val one = JsonObject()
+    one.addProperty("name", "one")
+    one.addProperty("group", "foo")
+    one.addProperty("tileWidth", 1)
+    one.addProperty("tileHeight", 1)
+    screenshots.add(one)
+
+    val two = JsonObject()
+    two.addProperty("name", "two")
+    two.addProperty("tileWidth", 1)
+    two.addProperty("tileHeight", 1)
+    screenshots.add(two)
+
+    val three = JsonObject()
+    three.addProperty("name", "three")
+    three.addProperty("group", "foo")
+    three.addProperty("tileWidth", 1)
+    three.addProperty("tileHeight", 1)
+    screenshots.add(three)
+
+    createTestMetadata(screenshots)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    val twoIndex = htmlContent.indexOf("two")
+    val oneIndex = htmlContent.indexOf("one")
+    val threeIndex = htmlContent.indexOf("three")
+
+    assertTrue("two should appear before one", twoIndex < oneIndex)
+    assertTrue("one should appear before three", oneIndex < threeIndex)
+  }
+
+  @Test
+  fun testGenerateHtml_splitPackageAndName() {
+    createTestMetadata(listOf(Triple("com.example.package.MyTest_testMethod", 1, 1)))
+    createTestImage("com.example.package.MyTest_testMethod.png", 10, 10, Color.BLUE)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue("HTML should contain package with demphasize", htmlContent.contains("demphasize\">com.example.package."))
+    assertTrue("HTML should contain test name", htmlContent.contains("MyTest_testMethod"))
+  }
+
+  @Test
+  fun testGenerateHtml_includesCommandButtons() {
+    createTestMetadata(listOf(Triple("test", 1, 1)))
+    createTestImage("test.png", 10, 10, Color.BLUE)
+
+    generateHtml(outputDir)
+
+    val htmlContent = File(outputDir, "index.html").readText()
+    assertTrue(
+      "HTML should have toggle dark button",
+      htmlContent.contains("Toggle Dark Background")
+    )
+    assertTrue(
+      "HTML should have toggle hierarchy button",
+      htmlContent.contains("Toggle View Hierarchy Overlay")
+    )
+  }
+
+  private fun createTestMetadata(screenshots: List<Triple<String, Int, Int>>) {
+    val metadata = JsonArray()
+    for ((name, tileWidth, tileHeight) in screenshots) {
+      val screenshot = JsonObject()
+      screenshot.addProperty("name", name)
+      screenshot.addProperty("tileWidth", tileWidth)
+      screenshot.addProperty("tileHeight", tileHeight)
+      metadata.add(screenshot)
+    }
+    createTestMetadata(metadata)
+  }
+
+  private fun createTestMetadata(metadata: JsonArray) {
+    val metadataFile = File(outputDir, "metadata.json")
+    metadataFile.writeText(metadata.toString())
+  }
+
+  private fun createTestImage(name: String, width: Int, height: Int, color: Color) {
+    val image = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+    val g = image.createGraphics()
+    try {
+      g.color = color
+      g.fillRect(0, 0, width, height)
+    } finally {
+      g.dispose()
+    }
+
+    val file = File(outputDir, name)
+    ImageIO.write(image, "PNG", file)
   }
 }
