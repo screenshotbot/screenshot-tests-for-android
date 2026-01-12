a1df630bf39bbf49b9b7097590dbdee849479244
Arnold Noronha <arnold@screenshotbot.io>
2026-01-12 10:01:19 -0500

    ...

--- plugin/src/main/kotlin/com/facebook/testing/screenshot/build/PullScreenshotsTask.kt
+++ plugin/src/main/kotlin/com/facebook/testing/screenshot/build/PullScreenshotsTask.kt
@@ -89,7 +89,7 @@ open class PullScreenshotsTask : ScreenshotTask() {
         pullImages(File(tempDir), deviceDir, testRunId, puller)
     }
 
-    copyAssets(tempDir);
+    copyAssets(File(tempDir));
 
     project.exec { execSpec ->
       execSpec.executable = extension.pythonExecutable
