3437cde9e8604339258b18a9ff360ca54112edab
Arnold Noronha <arnold@screenshotbot.io>
2026-01-12 11:50:46 -0500

    Deprecate the pythonExecuable setting
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D12220

--- plugin/src/main/kotlin/com/facebook/testing/screenshot/build/ScreenshotsPlugin.kt
+++ plugin/src/main/kotlin/com/facebook/testing/screenshot/build/ScreenshotsPlugin.kt
@@ -36,7 +36,10 @@ open class ScreenshotsPluginExtension {
   /** Whether to store screenshots in device specific folders */
   var multipleDevices = false
   /** The python executable to use */
+
+  @Deprecated("screenshot-tests-for-android no longer requires python")
   var pythonExecutable = "python"
+
   /** The directory to compare screenshots from in verify only mode */
   var referenceDir: String? = null
   /** The directory to save failed screenshots */
