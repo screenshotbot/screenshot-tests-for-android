8447279a17d44a132d1183fec43ef844ef1a56b9
Arnold Noronha <arnold@screenshotbot.io>
2026-01-15 11:47:04 -0500

    ...

---
+++ plugin/src/main/kotlin/com/facebook/testing/screenshot/build/AdditionalTestOutputPuller.kt
@@ -0,0 +1,19 @@
+package com.facebook.testing.screenshot.build
+
+class AdditionalTestOutputPuller : RemoteFilePuller {
+  override fun remoteFileExists(src: String): Boolean {
+    TODO("Not yet implemented")
+  }
+
+  override fun pull(src: String, dest: String) {
+    TODO("Not yet implemented")
+  }
+
+  override fun pullFolder(src: String, dest: String) {
+    TODO("Not yet implemented")
+  }
+
+  override fun getExternalDataDir(): String {
+    TODO("Not yet implemented")
+  }
+}
