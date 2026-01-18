078e197fb20c22fc037ccc98d032eacb89cb0e8a
Arnold Noronha <arnold@screenshotbot.io>
2026-01-18 16:57:15 -0500

    ...

--- sample/src/androidTest/java/com/facebook/testing/screenshot/sample/HelloWorldScreenshotTest.kt
+++ sample/src/androidTest/java/com/facebook/testing/screenshot/sample/HelloWorldScreenshotTest.kt
@@ -16,24 +16,13 @@
 
 package com.facebook.testing.screenshot.sample
 
-import android.widget.FrameLayout
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.ComposeView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.runtime.R
 import androidx.savedstate.SavedStateRegistry
 import androidx.savedstate.SavedStateRegistryOwner
 import androidx.test.annotation.UiThreadTest
-import androidx.test.platform.app.InstrumentationRegistry
-import com.facebook.testing.screenshot.Screenshot
-import com.facebook.testing.screenshot.ViewHelpers
-import com.facebook.testing.screenshot.WindowAttachment
 import org.junit.Test
 
-import androidx.lifecycle.testing.TestLifecycleOwner
 import androidx.savedstate.SavedStateRegistryController
 
 class TestSavedStateRegistryOwner(val testLifecycleOwner: LifecycleOwner) :
@@ -60,32 +49,4 @@ class HelloWorldScreenshotTest {
     }
   }
 
-  private fun performScreenshot(widthDp: Int, heightDp: Int, composable: @Composable () -> Unit) {
-    val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
-    val composeView = ComposeView(targetContext).apply {
-      setContent(composable)
-    }
-
-    val lifecycleOwner = TestLifecycleOwner()
-    lifecycleOwner.lifecycle.currentState = Lifecycle.State.INITIALIZED;
-    composeView.setTag(R.id.view_tree_lifecycle_owner, lifecycleOwner)
-
-    val savedStateRegistryOwner = TestSavedStateRegistryOwner(lifecycleOwner);
-
-    composeView.setTag(
-      androidx.savedstate.R.id.view_tree_saved_state_registry_owner,
-      savedStateRegistryOwner
-    );
-    val detacher = WindowAttachment.dispatchAttach(composeView);
-
-    ViewHelpers.setupView(composeView)
-      .setExactWidthDp(widthDp)
-      .setExactHeightDp(heightDp)
-      .layout()
-
-    lifecycleOwner.lifecycle.currentState = Lifecycle.State.STARTED;
-
-    Screenshot.snap(composeView).record()
-    detacher.detach();
-  }
 }
--- sample/src/androidTest/java/com/facebook/testing/screenshot/sample/performScreenshot.kt
+++ sample/src/androidTest/java/com/facebook/testing/screenshot/sample/performScreenshot.kt
@@ -1,2 +1,40 @@
-package com.facebook.testing.screenshot.sample 
+package com.facebook.testing.screenshot.sample
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.runtime.R
+import androidx.lifecycle.testing.TestLifecycleOwner
+import androidx.test.platform.app.InstrumentationRegistry
+import com.facebook.testing.screenshot.Screenshot
+import com.facebook.testing.screenshot.ViewHelpers
+import com.facebook.testing.screenshot.WindowAttachment
+
+fun performScreenshot(widthDp: Int, heightDp: Int, composable: @Composable () -> Unit) {
+  val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+  val composeView = ComposeView(targetContext).apply {
+    setContent(composable)
+  }
+
+  val lifecycleOwner = TestLifecycleOwner()
+  lifecycleOwner.lifecycle.currentState = Lifecycle.State.INITIALIZED;
+  composeView.setTag(R.id.view_tree_lifecycle_owner, lifecycleOwner)
+
+  val savedStateRegistryOwner = TestSavedStateRegistryOwner(lifecycleOwner);
+
+  composeView.setTag(
+    androidx.savedstate.R.id.view_tree_saved_state_registry_owner,
+    savedStateRegistryOwner
+  );
+  val detacher = WindowAttachment.dispatchAttach(composeView);
+
+  ViewHelpers.setupView(composeView)
+    .setExactWidthDp(widthDp)
+    .setExactHeightDp(heightDp)
+    .layout()
+
+  lifecycleOwner.lifecycle.currentState = Lifecycle.State.STARTED;
+
+  Screenshot.snap(composeView).record()
+  detacher.detach();
+}
