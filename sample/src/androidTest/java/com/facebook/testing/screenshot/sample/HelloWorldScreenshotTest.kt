1d660944a8d0d215c0379699f152d92f320b86f9
Arnold Noronha <arnold@screenshotbot.io>
2026-01-18 16:58:45 -0500

    Extract out the logic for Compose screenshotting
    Differential Revision: https://phabricator.tdrhq.com/D12280

--- sample/src/androidTest/java/com/facebook/testing/screenshot/sample/HelloWorldScreenshotTest.kt
+++ sample/src/androidTest/java/com/facebook/testing/screenshot/sample/HelloWorldScreenshotTest.kt
@@ -16,70 +16,19 @@
 
 package com.facebook.testing.screenshot.sample
 
-import android.widget.FrameLayout
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.platform.ComposeView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.savedstate.SavedStateRegistry
-import androidx.savedstate.SavedStateRegistryOwner
 import androidx.test.annotation.UiThreadTest
-import androidx.test.platform.app.InstrumentationRegistry
-import com.facebook.testing.screenshot.Screenshot
-import com.facebook.testing.screenshot.ViewHelpers
-import com.facebook.testing.screenshot.WindowAttachment
 import org.junit.Test
-
-import androidx.lifecycle.testing.TestLifecycleOwner
-import androidx.savedstate.SavedStateRegistryController
-
-class TestSavedStateRegistryOwner(val testLifecycleOwner: LifecycleOwner) :
-  SavedStateRegistryOwner {
-  private val controller = SavedStateRegistryController.create(this)
-
-  override val lifecycle = testLifecycleOwner.lifecycle
-  override val savedStateRegistry: SavedStateRegistry = controller.savedStateRegistry
-
-  init {
-    controller.performRestore(null)
-  }
-
-}
 
 class HelloWorldScreenshotTest {
   @Test
   @UiThreadTest
   fun testHelloWorld() {
-    val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
-    val composeView = ComposeView(targetContext).apply {
-      setContent {
-        MaterialTheme {
-          HelloWorld()
-        }
+    performScreenshot(300, 200) {
+      MaterialTheme {
+        HelloWorld()
       }
     }
-
-    val lifecycleOwner = TestLifecycleOwner()
-    lifecycleOwner.lifecycle.currentState = Lifecycle.State.INITIALIZED;
-    composeView.setTag(androidx.lifecycle.runtime.R.id.view_tree_lifecycle_owner, lifecycleOwner)
-
-    val savedStateRegistryOwner = TestSavedStateRegistryOwner(lifecycleOwner);
-
-    composeView.setTag(
-      androidx.savedstate.R.id.view_tree_saved_state_registry_owner,
-      savedStateRegistryOwner
-    );
-    val detacher = WindowAttachment.dispatchAttach(composeView);
-
-    ViewHelpers.setupView(composeView)
-      .setExactWidthDp(300)
-      .setExactHeightDp(200)
-      .layout()
-
-    lifecycleOwner.lifecycle.currentState = Lifecycle.State.STARTED;
-
-    Screenshot.snap(composeView).record()
-    detacher.detach();
   }
+
 }
