716686db39b5293108db0845910d243158740992
Arnold Noronha <arnold@screenshotbot.io>
2026-01-19 18:11:07 -0500

    Add a simple happy path test for a compose component
    Summary: ...
    
    Differential Revision: https://phabricator.tdrhq.com/D12290

---
+++ compose/src/androidTest/java/com/facebook/testing/screenshot/compose/HelloWorldScreenshotTest.kt
@@ -0,0 +1,28 @@
+package com.facebook.testing.screenshot.compose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.test.annotation.UiThreadTest
+import org.junit.Test
+
+class HelloWorldScreenshotTest {
+  @Test
+  @UiThreadTest
+  fun testHelloWorld() {
+    screenshot(300, 200) {
+      Box(
+        modifier = Modifier
+          .fillMaxSize()
+          .background(Color.White),
+        contentAlignment = Alignment.Center
+      ) {
+        BasicText("Hello, World!")
+      }
+    }
+  }
+}
